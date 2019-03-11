c415f4f9b50ff0eb2e84a97fab990b8eab48744e
Veetaha <gerzoh1@gmail.com>
2019-03-11 23:09:24 +0200

    removed ClassType and ClassPrototype interfaces from /common/interfaces as 'vee-type-safe' defines them by itself

--- common/interfaces.ts
+++ common/interfaces.ts
@@ -1,7 +1,1 @@
 export { Maybe } from 'vee-type-safe';
-
-export interface ClassType<TInstance = any, TArgs extends any[] = any[]> extends Function  {
-    new (...args: TArgs): TInstance;
-}
-
-export type ClassPrototype<TClass extends ClassType = ClassType> = TClass['prototype'];
